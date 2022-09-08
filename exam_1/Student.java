c738f97aac78e713384ff066e880df0632a445f8
Ara Gonzaga <aramazing318@gmail.com>
2022-09-08 14:31:55 +0800

    Create Student.java

---
+++ exam_1/Student.java
@@ -0,0 +1,19 @@
+public class Student {
+    String name;
+    String dateofbirth;
+    int studentnumber;
+    String emailadd;
+    boolean iAmAwesome;
+
+    public void sayMyCanbeStudentnumber() {
+        System.out.println("My student number is " + studentnumber);
+
+    }
+    public void sayMyEmailadd() {
+        System.out.println("My email address is " + emailadd);
+
+    }
+    public void sayMyIamAwesome() {
+    System.out.println("I am awesome: " + iAmAwesome);
+    }
+}
