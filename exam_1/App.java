12700893e68101846ecc4de0ba66e611a6f2b4cb
Ara Gonzaga <aramazing318@gmail.com>
2022-09-08 14:31:29 +0800

    Create App.java

---
+++ exam_1/App.java
@@ -0,0 +1,15 @@
+public class App {
+    public static void main(String[] args) {
+
+      Student me = new Student();
+
+        me.name = "Gonzaga, Arabella Gracie P.";
+        me.dateofbirth = "03/18/2004";
+        me.studentnumber = 2022102219;
+        me.emailadd = "gonzagaap@student.national-u.edu.ph";
+        me.iAmAwesome = true;
+        me.sayMyCanbeStudentnumber();
+        me.sayMyEmailadd();
+        me.sayMyIamAwesome();
+    }
+}
